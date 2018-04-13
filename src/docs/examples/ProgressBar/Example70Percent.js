12509b77276248c611f1589af7afad3fec124f3f
Filippakis George <g.filippakis@intralot.com>
2018-04-13 11:58:10 +0300

    Atom - ProgressBar

---
+++ src/docs/examples/ProgressBar/Example70Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+/** 70% progress */
+export default function Example10Percent() {
+    return <ProgressBar percent={70} width={150} />
+}
