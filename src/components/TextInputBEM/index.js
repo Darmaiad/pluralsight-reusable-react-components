6f26c95aeedb1ac13683e48ad3e54860f851ce72
Filippakis George <g.filippakis@intralot.com>
2018-04-13 15:27:22 +0300

    TextInput using CSS BEM notation

---
+++ src/components/TextInputBEM/index.js
@@ -0,0 +1,6 @@
+// importing the stylesheet here offers to options for importing the component:
+//      - If someone wants to import the styles, he can import from this index.
+//      - If someone does not want the styles he can import the component directly.
+
+import './textInput.css';
+export { default } from './TextInputBEM';
